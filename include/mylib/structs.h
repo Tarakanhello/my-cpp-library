9c97f2e54a750e8ffa990207c47ebb65ebb5e95d
Tarakanhello <tarakanhello@gmail.com>
2026-06-15 16:08:22 +0300

    add comments

--- include/mylib/structs.h
+++ include/mylib/structs.h
@@ -109,82 +109,161 @@ namespace mylib
         }
     };
 
+    /**
+     * @brief Компаратор, преобразующий объекты перед сравнением.
+     * @tparam T          Исходный тип.
+     * @tparam TRANSFORM  Функтор преобразования (принимает T, возвращает некоторый тип U).
+     * @tparam COMPARATOR Компаратор для преобразованных значений (по умолчанию DefaultComparator<U>).
+     */
     template<typename T, typename TRANSFORM, typename COMPARATOR>
     struct TransformComparator
     {
         TRANSFORM transform;
         COMPARATOR comparator;
 
+        /**
+         * @brief Конструктор.
+         * @param theTransform   Функтор преобразования (по умолчанию создаётся новый).
+         * @param theComparator  Компаратор для преобразованных значений (по умолчанию новый).
+         */
         explicit TransformComparator(const TRANSFORM& theTransform = TRANSFORM(),
                                      const COMPARATOR& theComparator = COMPARATOR())
             : transform{ theTransform }
             , comparator{ theComparator }
         {}
 
+         /**
+         * @brief Сравнение после применения transform.
+         */
         bool operator()(const T& left, const T& right) const
         {
             return comparator(transform(left), transform(right));
         }
 
+        /**
+         * @brief Проверка равенства после применения transform.
+         */
         bool isEqual(const T& left, const T& right) const
         {
             return comparator.isEqual(transform(left), transform(right));
         }
     };
 
+    /**
+     * @brief Функтор преобразования указателя в ссылку.
+     * @tparam T Тип хранимого значения.
+     *
+     * @warning Оператор вызова содержит assert(ptr). Если передан нулевой указатель,
+     *          программа аварийно завершится.
+     */
     template<typename T>
     struct PointerTransform
     {
         const T& operator()(const T* item) const
         {
-            assert(item); // здесь приложение упадет при ошибке. Насколько это оправдано, или лучше выбросить исключение?
+            assert(item);
             return *item;
         }
     };
 
+    /**
+     * @brief Удобный псевдоним для сравнения указателей через PointerTransform.
+     * @tparam T Тип значения, на которое указывает указатель.
+     * @tparam COMPARATOR Компаратор для значений T (по умолчанию DefaultComparator<T>).
+     */
     template<typename T, typename COMPARATOR = DefaultComparator<T> >
     using PointerComparator = TransformComparator<const T*, PointerTransform<T>, COMPARATOR>;
 
+    /**
+     * @brief Функтор, преобразующий индекс в элемент массива.
+     * @tparam T Тип элементов массива.
+     */
     template<typename T>
     struct IndexTransform
     {
         const T* array;
+
+        /**
+         * @brief Конструктор, запоминающий массив.
+         * @param theArray Указатель на массив (должен существовать дольше, чем объект трансформации).
+         */
         explicit IndexTransform(const T* theArray)
             : array{ theArray }
         {}
 
+        /**
+         * @brief Доступ к элементу по индексу.
+         * @param i Индекс.
+         * @return Ссылка на i-й элемент.
+         */
         const T& operator()(std::size_t i) const
         {
             return array[i];
         }
     };
 
+    /**
+     * @brief Компаратор для сравнения элементов массива по индексам.
+     * @tparam T Тип элементов массива.
+     * @tparam COMPARATOR Компаратор для значений T (по умолчанию DefaultComparator<T>).
+     */
     template<typename T, typename COMPARATOR = DefaultComparator<T> >
     using IndexComparator = TransformComparator<std::size_t, IndexTransform<T>, COMPARATOR>;
 
+    /**
+     * @brief Функтор, извлекающий первый элемент из std::pair.
+     * @tparam FIRST  Тип первого элемента пары.
+     * @tparam SECOND Тип второго элемента пары.
+     */
     template<typename FIRST, typename SECOND>
     struct PairFirstTransform
     {
+        /**
+         * @brief Возвращает pair.first.
+         * @return Константная ссылка на поле first.
+         */
         const FIRST& operator()(const std::pair<FIRST, SECOND>& pair)
         {
             return pair.first;
         }
     };
 
+    /**
+     * @brief Компаратор, сравнивающий std::pair по первому элементу.
+     * @tparam FIRST     Тип первого элемента.
+     * @tparam SECOND    Тип второго элемента (не участвует в сравнении).
+     * @tparam COMPARATOR Компаратор для FIRST.
+     */
     template<typename FIRST, typename SECOND, typename COMPARATOR = DefaultComparator<FIRST> >
     using PairFirstComparator = TransformComparator<std::pair<FIRST, SECOND>,
                                                     PairFirstTransform<FIRST, SECOND>,
                                                     COMPARATOR>;
 
+    /**
+     * @brief Лексикографический компаратор для контейнеров, поддерживающих operator[] и size().
+     * @tparam VECTOR Тип контейнера (например, std::vector, std::string).
+     *
+     * Предоставляет сравнение двух контейнеров как последовательностей,
+     * а также вспомогательные методы для поэлементного сравнения.
+     */
     template<typename VECTOR>
     struct LexicographicComparator
     {
+        /**
+         * @brief Сравнение элементов на позиции i (без проверки границ).
+         * @return true если левый элемент меньше правого на позиции i,
+         *         иначе false (если индексы вне диапазона – см. логику).
+         */
         bool operator()(const VECTOR& left, const VECTOR& right, std::size_t i) const
         {
             return i < left.size() ? i < right.size() && left[i] < right[i] :
                                         i < right.size();
         }
 
+        /**
+         * @brief Лексикографическое сравнение двух контейнеров.
+         * @return true если left < right в лексикографическом порядке.
+         */
         bool operator()(const VECTOR& left, const VECTOR& right) const
         {
             for(std::size_t i{}; i < std::min(left.size(), right.size()); ++i)
@@ -201,12 +280,19 @@ namespace mylib
             return left.size() < right.size();
         }
 
+        /**
+         * @brief Проверка равенства элементов на позиции i.
+         */
         bool isEqual(const VECTOR& left, const VECTOR& right, std::size_t i) const
         {
             return i < left.size() ? i < right.size() && left[i] == right[i] :
                                         i >= right.size();
         }
 
+        /**
+         * @brief Проверка полного равенства двух контейнеров.
+         * @return true если left и right имеют одинаковый размер и все элементы равны.
+         */
         bool isEqual(const VECTOR& left, const VECTOR& right) const
         {
             for(std::size_t i{}; i < std::min(left.size(), right.size()); ++i)
@@ -220,12 +306,25 @@ namespace mylib
         }
 
 
+        /**
+         * @brief Возвращает размер контейнера (нужен для некоторых алгоритмов).
+         */
         std::size_t getSize(const VECTOR& vector) const
         {
             return vector.size();
         }
     };
 
+    /**
+     * @brief Находит индекс минимального элемента в массиве.
+     * @tparam T Тип элементов.
+     * @tparam COMPARATOR Тип компаратора (по умолчанию DefaultComparator<T>).
+     * @param array Указатель на массив.
+     * @param size  Количество элементов.
+     * @param comparator Компаратор, определяющий отношение "меньше".
+     * @pre size > 0, array != nullptr.
+     * @return Индекс первого вхождения минимального элемента.
+     */
     template<typename T, typename COMPARATOR = DefaultComparator<T>>
     std::size_t argMin(const T* array, std::size_t size, const COMPARATOR& comparator = COMPARATOR())
     {
@@ -243,12 +342,28 @@ namespace mylib
         return best;
     }
 
+    /**
+     * @brief Находит индекс максимального элемента в массиве (через обратный компаратор).
+     * @tparam T Тип элементов.
+     * @tparam COMPARATOR Компаратор (по умолчанию ReverseComparator<T>).
+     * @param array Указатель на массив.
+     * @param size  Количество элементов.
+     * @param comparator Компаратор (обычно игнорируется, т.к. используется ReverseComparator).
+     * @return Индекс максимального элемента.
+     */
     template<typename T, typename COMPARATOR = ReverseComparator<T>>
     std::size_t argMax(const T* array, std::size_t size, const COMPARATOR& comparator = COMPARATOR())
     {
         return argMin(array, size, comparator);
     }
 
+    /**
+     * @brief Возвращает ссылку на минимальный элемент массива (неконстантная версия).
+     * @tparam T Тип элементов.
+     * @param array Указатель на массив.
+     * @param size  Количество элементов.
+     * @return Ссылка на минимальный элемент.
+     */
     template<typename T>
     T& valMin(T* array, std::size_t size)
     {
@@ -257,6 +372,7 @@ namespace mylib
         return array[index];
     }
 
+    /// @copydoc valMin (константная версия)
     template<typename T>
     const T& valMin(const T* array, std::size_t size)
     {
@@ -265,6 +381,9 @@ namespace mylib
         return array[index];
     }
 
+    /**
+     * @brief Возвращает ссылку на максимальный элемент массива (неконстантная версия).
+     */
     template<typename T>
     T& valMax(T* array, std::size_t size)
     {
@@ -273,6 +392,7 @@ namespace mylib
         return array[index];
     }
 
+    /// @copydoc valMax (константная версия)
     template<typename T>
     const T& valMax(const T* array, std::size_t size)
     {
@@ -281,7 +401,16 @@ namespace mylib
         return array[index];
     }
 
-    // индекс минимума массива с функцией преобразования
+
+    /**
+     * @brief Находит индекс минимального элемента, используя функцию преобразования.
+     * @tparam T Тип элементов.
+     * @tparam FUNCTION Тип функтора, принимающего T и возвращающего некоторый тип для сравнения.
+     * @param array Указатель на массив.
+     * @param size  Количество элементов.
+     * @param function Функтор преобразования (например, доступ к полю).
+     * @return Индекс элемента, для которого значение function(element) минимально.
+     */
     template<typename T, typename FUNCTION>
     std::size_t argMinFunc(const T* array, std::size_t size, const FUNCTION& function)
     {
@@ -291,6 +420,9 @@ namespace mylib
         return argMin(array, size, comp);
     }
 
+    /**
+     * @brief Возвращает ссылку на элемент, с минимальным значением.
+     */
     template<typename T, typename FUNCTION>
     T& valMinFunc(T* array, std::size_t size, const FUNCTION& function)
     {
@@ -301,6 +433,14 @@ namespace mylib
 
     } // end namespace comparators
 
+    /**
+     * @brief Класс-примесь (mixin), добавляющий арифметические операторы.
+     * @tparam T Наследуемый тип (CRTP).
+     *
+     * Чтобы добавить своему классу операторы +, -, *, /, %, <<, >>,
+     * достаточно унаследовать от ArithmeticType<YourClass> и реализовать
+     * составные операторы +=, -=, *=, /=, %=, <<=, >>=.
+     */
     template<typename T>
     struct ArithmeticType
     {
