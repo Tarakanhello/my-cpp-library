81bdf7ebeab5796e026c59fa3a2060ea95291c25
Tarakanhello <tarakanhello@gmail.com>
2026-07-01 16:42:34 +0300

    add construct and destroy to MyAllocator

--- include/mylib/memory.h
+++ include/mylib/memory.h
@@ -1,8 +1,10 @@
 #ifndef MEMORY_H_INCLUDED
 #define MEMORY_H_INCLUDED
 
-#include <new>
 #include <cstddef>
+#include <limits>
+#include <new>
+#include <utility>
 
 namespace mylib
 {
@@ -63,8 +65,17 @@ namespace mylib
                 return nullptr;
             }
 
-            return static_cast<T*>(::operator new(sizeof(T) * size,
-                                                   std::align_val_t{ alignof(T) }));
+            if(size > std::numeric_limits<size_t>::max() / sizeof(T))
+            {
+                throw std::bad_alloc();
+            }
+            if(auto p {static_cast<T*>(::operator new(sizeof(T) * size,
+                                                   std::align_val_t{ alignof(T) })) })
+            {
+                return p;
+            }
+
+            throw std::bad_alloc();
         }
 
 
@@ -76,6 +87,21 @@ namespace mylib
                 array = nullptr;
             }
         }
+
+        // Конструирование объекта в выделенной памяти (placement new)
+        template< typename U, typename... ARGS>
+        void construct(U* place, ARGS&&... args)
+        {
+            new (place) U(std::forward<ARGS>(args)...);
+        }
+
+        // Уничтожение объекта без освобождения памяти
+        template<typename U>
+        void destroy(U* p) noexcept
+        {
+            p->~U();
+        }
+
     };
 
     template<typename T, typename U>
