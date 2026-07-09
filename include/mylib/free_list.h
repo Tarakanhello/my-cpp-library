f72b3109c8f0b19b0f982c0bf2ed974abc8c975e
Tarakanhello <tarakanhello@gmail.com>
2026-07-09 16:52:58 +0300

    thinking about implementation

--- include/mylib/free_list.h
+++ include/mylib/free_list.h
@@ -4,96 +4,78 @@
 #include "mylib/list.h"
 #include "mylib/memory.h"
 
-template<typename T, typename ALLOCATOR = mylib::MySimpleAllocator<T>>
-class FreeList final
+namespace mylib
 {
-private:
-    static constexpr size_t MIN_BLOCK_SIZE{ 8 };
-    static constexpr size_t MAX_BLOCK_SIZE{ 8192 };
-    static constexpr size_t DEFAULT_SIZE{ 32 };
-
-
-    struct Block
+    template<typename T, typename ALLOCATOR = mylib::MySimpleAllocator<T>>
+    class FreeList final
     {
-        size_t capacity;    // общее количество ячеек в блоке
-        size_t size;        // текущее количество занятых ячеек
-        size_t maxSize;     // максимальный индекс выделенных ячеек (для новых)
+    private:
+        static constexpr size_t MIN_BLOCK_SIZE{ 8 };
+        static constexpr size_t MAX_BLOCK_SIZE{ 8192 };
+        static constexpr size_t DEFAULT_SIZE{ 32 };
 
-        struct Item
+        struct Block;
+
+        List<Block, ALLOCATOR> m_blocks;
+
+        T* allocate()
         {
-            T item;
-            union
-            {
-                T* next; // для свободных ячеек
-                void* userData; // для занятых (указатель на Block)
-            };
-        };
 
-        Item* nodes;    // массив Item
-        Item* userData; // массив Item
+        }
 
-        Block(size_t cap);
-        ~Block();
-        // Создаём массив флагов размером maxSize, все ставим в true
-        // (значит, подлежат уничтожению).
-        // Проходим по списку возвращённых ячеек и помечаем их как false
-        // (они уже уничтожены, их деструкторы вызывать не надо).
-        // Затем проходим по всем ячейкам от 0 до maxSize-1 и, если флаг true,
-        // вызываем деструктор объекта.
-        // После этого освобождаем всю память массива.
-
-        bool isFull() const { return size == capacity; }
-        bool isEmpty() const { return size == 0; }
-        T* allocate();
-        // Сначала проверяем, есть ли ячейка в списке возвращённых.
-        // возвращенная ячейка - ранее использовавшаяся, но сейчас пустая (удаленная)
-        // Если есть — берём первую и удаляем её из списка.
-        // Если список пуст — значит, все ранее выделенные ячейки заняты,
-        // и мы берём новую из ещё не использованной части массива
-        // (с индексом maxSize), увеличивая maxSize.
-        // Если массив полностью заполнен (size == capacity),
-        // выделение невозможно — нужно создать новый блок
-        // (это уже динамический вариант).
-
-        void remove(Item* node);
-        // Вызываем деструктор объекта.
-        // Добавляем ячейку в начало списка возвращённых
-        // (просто перенаправляем указатель next).
-        // Уменьшаем size (количество занятых ячеек).
-
-        void collectGarbage(); // вызывается в деструкторе блока
     };
 
-    using BlockList = mylib::List<Block, ALLOCATOR>;
 
-    BlockList blocks;
-    size_t blockSise{}; // размер для нового блока
-    void createNewBlock();
+    template<typename T, typename ALLOCATOR>
+    struct FreeList<T, ALLOCATOR>::
+        Block final
+    {
+        struct Item;
 
-public:
-    explicit FreeList(size_t initialSize = DEFAULT_SIZE, ALLOCATOR alloc = ALLOCATOR());
-    ~FreeList();
+        using ITEM_ALLOCATOR = typename std::allocator_traits<ALLOCATOR>::template rebind_alloc<Item>;
+        using ALLOC_TRAITS = std::allocator_traits<ITEM_ALLOCATOR>;
+
+        Item* returned; // head of free Items;
+        Item* unused;   // unused part
+
+        ITEM_ALLOCATOR itemAllocator;
+
+        // size <= maxSize <= capacity
+        size_t size{};
+        size_t maxSize{};
+        size_t capacity{};
+
+        Block(size_t theCapacity, ALLOCATOR alloc = ALLOCATOR())
+            : capacity{ theCapacity }
+            , size{}
+            , maxSize{}
+            , returned{ nullptr }
+            , itemAllocator{ alloc }
+            , unused{ ALLOC_TRAITS::allocate(itemAllocator, theCapacity) }
+        {}
 
 
-    FreeList(const FreeList&) = delete;
-    FreeList& operator=(const FreeList&) = delete;
+        bool isFull() const { return size == capacity; }
+        bool empty() const { return size == 0; }
 
-    T* allocate();
-    // Берём первый блок в списке. Если он полон или список пуст,
-    // создаём новый блок (с текущим размером, который увеличивается вдвое)
-    // и добавляем его в начало.
-    // Выделяем ячейку из этого блока.
+        explicit operator bool() const noexcept { return !empty(); }
+    };
 
 
-    void remove(T* item);
-    // По указателю на объект определяем, из какого блока он родом
-    // (для этого в объекте хранится ссылка на блок).
-    // Возвращаем ячейку в этот блок.
-    // Если блок стал пустым, удаляем его (память освобождается).
-    // Если блок не пуст, перемещаем его в начало списка
-    // (чтобы неполные блоки были впереди).
-    // Если блок стал неполным, но был полным, тоже перемещаем в начало.
+    template<typename T, typename ALLOCATOR>
+    struct FreeList<T, ALLOCATOR>::Block::
+        Item final
+    {
+        T item;
 
-};
+        union
+        {
+            Item* next; // *this is free, pointer to next free Item;
+            void* userData; // *this is full, pointer to owner of this Item;
+        };
+    };
+
+} // end namespace mylib;
+
 
 #endif // FREE_LIST_H
