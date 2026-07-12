afba5c4cb0792d8d15383ad2e76134d013e26432
Tarakanhello <tarakanhello@gmail.com>
2026-07-12 19:24:34 +0300

    Add skeleton of FreeList class with block management, allocation and removal interfaces

--- include/mylib/free_list.h
+++ include/mylib/free_list.h
@@ -1,9 +1,13 @@
 #ifndef FREE_LIST_H
 #define FREE_LIST_H
 
+#include <cstddef>
+
 #include "mylib/list.h"
 #include "mylib/memory.h"
-#include "mylib/vector.h"
+#include "mylib/static_free_list.h"
+
+
 
 namespace mylib
 {
@@ -11,9 +15,46 @@ namespace mylib
     class FreeList final
     {
     private:
-        static constexpr size_t MIN_BLOCK_SIZE{ 8 };
-        static constexpr size_t MAX_BLOCK_SIZE{ 8192 };
-        static constexpr size_t DEFAULT_SIZE{ 32 };
+        enum class BlockSizes
+        {
+            MIN_BLOCK_SIZE = 8,
+            MAX_BLOCK_SIZE = 8192,
+            DEFAULT_SIZE = 32,
+            UNDEFAULT_SIZE = 0
+        };
+
+    private:
+        using Block = StaticFreeList<T, ALLOCATOR>;
+        using BlockAllocator = typename std::allocator_traits<ALLOCATOR>::template rebind_alloc<Block>;
+        using BlockList = mylib::List<Block, BlockAllocator>;
+
+        BlockList m_blocks{};
+        size_t m_currentBlockSize{ BlockSizes::MIN_BLOCK_SIZE };
+        size_t m_totalSize{};
+
+
+        Block* getFirstNonFullBlock();
+        void createNewBlock();
+        void moveBlockToFront(typename BlockList::iterator it);
+
+    public:
+        FreeList() = default;
+        explicit FreeList(size_t initialSize = BlockSizes::DEFAULT_SIZE, ALLOCATOR alloc = MySimpleAllocator<T>());
+
+        FreeList(const FreeList&) = delete;
+        FreeList& operator=(const FreeList&) = delete;
+
+        FreeList(FreeList&& other) noexcept;
+        FreeList& operator=(FreeList&& other) noexcept;
+
+        ~FreeList() noexcept;
+
+        T* allocate();
+        void remove(T* ptr);
+
+        bool empty() const noexcept { return m_blocks.empty(); }
+        size_t size() const noexcept { return m_totalSize; }
+        size_t blockCount() const noexcept { return m_blocks.size(); }
     };
 
 
